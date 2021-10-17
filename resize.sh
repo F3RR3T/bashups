ad4be3dd58ab5d86fb9c55c0633c94ef6d85030a
St33v <github@f3rr3t.com>
2021-10-17 15:19:27 +1100

    Add some error checking for reduction percentage

--- resize.sh
+++ resize.sh
@@ -6,6 +6,18 @@
 if [ -z $1 ] 
 	then reduce=25
 	else reduce=$1
+        re='^[0-9]+$'       # reg expression for digits only
+        if ! [[ $reduce =~ $re ]] 
+            then
+                echo "Usage: If you use arguments, arg 1 (resize percentage) must be an integer"
+                exit 1
+            else
+                # arg1 is an integer
+                if [ $reduce -gt 99 ] || [ $reduce -lt 1 ] ; then
+                    echo "Usage: reduction percentage must be between 1 and 99 percent"
+                    exit 1
+                fi
+        fi
 fi
 
 # Check for folder name in command line (no spaces of course!)
