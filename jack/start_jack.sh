03d04c0c849394a5c2d6e4a7fa26213ec637a149
St33v <github@f3rr3t.com>
2021-11-09 10:47:19 +1100

    seems to work. Let's call it release 1 eh

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -5,12 +5,13 @@
 
 jack_control start
 jack_control ds alsa
-jack_control dps device hw:HD2
+# cat /proc/asound/cards
+jack_control dps device hw:PCH  # I think should be the audio playback device
 jack_control dps rate 48000
-jack_control dps nperiods 2
+jack_control dps nperiods 2     # 3 for USB (mod from '2' default)
 jack_control dps period 64
 sleep 10
-a2j_control --ehw
+a2j_control --ehw               # export all available ALSA MIDI ports to JACK MIDI ports
 a2j_control --start
 sleep 10
 qjackctl &
