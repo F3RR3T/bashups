628c8d8eee29591f9fe96d2d734852962ce408e6
St33v <github@f3rr3t.com>
2021-11-09 11:12:13 +1100

    well it works, but it could be augmented later, Ron

--- super.sh
+++ super.sh
@@ -5,4 +5,17 @@
 
 theDate=$(\date -I)     # ISO 8601, natch
 
-:wq
+# Where to store the log file
+logFile=~/box/leger/etc/superLog.txt
+
+
+# to log a value, the user must include it as an argument
+if [ -z $1 ]
+    then 
+        echo "Usage: provide an argument for super.sh to append to the log"
+        exit 1
+fi
+
+echo $theDate   $1 >> $logFile
+
+
