572ab720e51cfa6b0cecb30b4dde4d7261ec54f5
St33v <github@f3rr3t.com>
2022-01-26 16:06:52 +1100

    something

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -3,13 +3,17 @@
 # https://wiki.archlinux.org/title/JACK_Audio_Connection_Kit
 # SJP 27 Oct 2021
 
+# logfile is at ~/.log/jack/jackdbus.log
+
+
+
 jack_control start
 jack_control ds alsa
 # cat /proc/asound/cards
 jack_control dps device hw:PCH  # I think should be the audio playback device
 jack_control dps rate 48000
 jack_control dps nperiods 2     # 3 for USB (mod from '2' default)
-jack_control dps period 64
+jack_control dps period 128
 sleep 10
 a2j_control --ehw               # export all available ALSA MIDI ports to JACK MIDI ports
 a2j_control --start
