8434fc0b33fc469c7d65d3559a306ecc09473318
St33v <stephen.pratt@calamia.com.au>
2022-01-26 14:26:17 +1100

    Update speakerWatchdog.sh

--- speakerWatchdog.sh
+++ speakerWatchdog.sh
@@ -10,3 +10,9 @@
 # st33v@cr4y:~$ grep RUNNING /proc/asound/card*/pcm*/sub*/status
 # /proc/asound/card2/pcm0p/sub0/status:state: RUNNING
 
+# That is fine, but the status remains as 'RUNNING' even after the speakers have turned themselves off...
+# So I'm going down the track of periodically sending a 'sound' to a loopback device
+# sudo modprobe snd-aloop
+# Now aply -l shows a new 'card' called Loopback.
+
+
